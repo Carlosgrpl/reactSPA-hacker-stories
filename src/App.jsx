e2adf613a3ee613bfa334602ab7b91f4a2425f8f
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-06-15 18:22:09 +0200

    Moving on to lists in Reactgit add ., also loving how JSX mixes JS and HTML seamlessly

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
 
-const title = "React";
+const welcome = {
+  title: "React",
+  greeting: "Hey there",
+};
 
+function getTitle(title) {
+  return title;
+}
 function App() {
   return (
     <div>
-      <h1>Hello {title}!</h1>
-      <label htmlFor="search">Search:</label> /* This is how we display HTML in
-      JSX */
-      <input id="search" type="text" /> /* Input is an HTML element, so we use
-      JSX with the id from htmlFor */
+      <h1>
+        {welcome.greeting} {getTitle("React")}
+      </h1>
+
+      <label htmlFor="search">Search:</label>
+
+      <input id="search" type="text" />
     </div>
   );
 }
